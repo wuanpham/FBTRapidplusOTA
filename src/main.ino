713c3f7c1e29cf8e18f873da0f49dc631610d6fe
Khang <nguyenhoangkhang20188@gmail.com>
2025-12-05 16:07:12 +0700

    test

--- src/main.ino
+++ src/main.ino
@@ -20,7 +20,7 @@ buttonManager _buttonManager;
 
 WebServer server(80);
 
-int currentVersion = 11;
+int currentVersion = 0;
 
 String baseUrl = "https://raw.githubusercontent.com/wuanpham/FBTRapidplusOTA/" + FirmwareVer + "/";
 String checkFile = "updateOTA.json";
